45b3a544c5e391a08e8139423b233252309e251a
ShchepetkovDY <114948795+ShchepetkovDY@users.noreply.github.com>
2023-02-13 19:20:27 +0300

    Homework done

--- part1/practice/bad_smell_laizy_class/main.py
+++ part1/practice/bad_smell_laizy_class/main.py
@@ -36,3 +36,29 @@ class Main:
         self.field_adapter = FieldAdapter(field=self.field)
         self.unit = Unit()
         self.unit.move(field_adapter=self.field_adapter)
+
+
+class Unit:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def attack(self):
+        pass
+
+    def defense(self):
+        pass
+
+    def move(self, field):
+        field.set_unit(x=self.x, y=self.y, unit=self)
+
+
+class Field:
+    def set_unit(self, x, y, unit: Unit):
+        pass
+
+class Main:
+    def __init__(self):
+        self.field = Field()
+        self.unit = Unit()
+        self.unit.move(field=self.field)
--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -48,3 +48,39 @@ class Unit:
             field.set_unit(x=new_x, y=new_y, unit=self)
 
 #     ...
+
+class Unit:
+    def __init__(self, field, x_coord: int, y_coord: int, direction, is_fly: bool, is_crawl: bool, speed: int = 1):
+        self.field = field
+        self.x_cord = x_coord
+        self.y_cord = y_coord
+        self.direction = direction
+        self.is_fly = is_fly
+        self.is_crawl = is_crawl
+        self.speed = speed
+
+    def get_speed(self):
+        if self.is_fly and self.is_crawl:
+            raise ValueError('Рожденный ползать летать не должен!')
+
+        elif self.is_fly:
+            self.speed *= 1.2
+
+        elif self.is_crawl:
+            self.speed *= 0.5
+
+    def move(self, direction):
+        speed = self.get_speed()
+        if direction == 'UP':
+            new_y = self.y_coord + speed
+            new_x = self.x_coord
+        elif direction == 'DOWN':
+            new_y = self.y_coord - speed
+            new_x = self.x_coord
+        elif direction == 'LEFT':
+            new_y = self.y_coord
+            new_x = self.x_coord - speed
+        elif direction == 'RIGTH':
+            new_y = self.y_coord
+            new_x = self.x_coord + speed
+        self.field.set_unit(x=new_x, y=new_y, unit=self)
